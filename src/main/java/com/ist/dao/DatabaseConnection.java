b4dbbe8f58b08f729b765dba0f8417e3446003a3
Sumit Ojha <102379532+sumitojha2002@users.noreply.github.com>
2025-09-21 12:22:28 +0545

    database connector implemented

--- src/main/java/com/ist/dao/DatabaseConnection.java
+++ src/main/java/com/ist/dao/DatabaseConnection.java
@@ -1,5 +1,24 @@
 package com.ist.dao;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DatabaseConnection {
-    
+    public static Connection getConnection(){
+    try{
+
+        Dotenv dotenv = Dotenv.load();
+        
+        String url = dotenv.get("DB_URL");
+        String user = dotenv.get("DB_USER");
+        String password = dotenv.get("DB_PASSWORD");
+        
+        return DriverManager.getConnection(url, user, password);
+    }catch(Exception e){
+        e.printStackTrace();
+        return null;
+    }
+  }
 }
+        
