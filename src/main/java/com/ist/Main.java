47ec000827b1f13dd7d0857028ce29269c6ba43a
Sumit Ojha <102379532+sumitojha2002@users.noreply.github.com>
2025-09-29 06:33:54 +0545

    added dashboard.fxml

--- src/main/java/com/ist/Main.java
+++ src/main/java/com/ist/Main.java
@@ -1,7 +1,22 @@
 package com.ist;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        Group root = new Group();
+        Scene scene = new Scene(root);
+        root.getChildren().add(new Button("Click me!"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
     }
 }
